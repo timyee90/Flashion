112ffd7ef240f92395e7c449b716eeddd3896026
Sina Nikmaram <NikmaramCareer@gmail.com>
2020-05-11 15:26:11 -0400

    Directory Init

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require("webpack");
+const path = require("path");
+
+const config = {
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
+
+module.exports = config;
