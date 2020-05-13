181c142c60bf2d76c1b50be5e74d6f5e93e2e272
Tim <timyee90@gmail.com>
2020-05-13 16:26:19 -0400

    fix division by 0 bug for no reviews

--- utils/computeRatingAverage.js
+++ utils/computeRatingAverage.js
@@ -2,6 +2,6 @@ export const computeAverageRating = ({ results }) => {
   return (
     results.reduce((a, b) => {
       return a.rating + b.rating;
-    }, 0) / results.length
+    }, 0) / results.length || 0
   );
 };
