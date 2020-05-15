1819ac728c95649f9be16a46719bdb2d56738ae4
jamesjeong94 <jamesjeong94@gmail.com>
2020-05-14 23:34:35 -0400

    added progressBar

---
+++ client/components/ProgressBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ProgressBar = (props) => {
+  const progressBarStyle = {
+    position: 'relative',
+    height: '15px',
+    width: '100px',
+    'border-radius': '25%',
+    border: '1px solid black',
+  };
+  const fillerStyle = {
+    backgroundColor: 'black',
+    height: '100%',
+    'border-radius': 'inherit',
+    width: props.percentage,
+  };
+  return (
+    <div className='progress-bar' style={progressBarStyle}>
+      <div className='filler' style={fillerStyle}></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
